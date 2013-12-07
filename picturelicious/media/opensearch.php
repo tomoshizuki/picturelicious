92b1d9dbd6501555e69bb0eebbafdc2c30546b60
Norbert Fischer <fischer@0x90.io>
2013-12-07 12:23:53 +0100

    Removed the method parameter for the search plugin
    I couldn't find the method parameter within the opensearch specs but only at the
    MDN and it also breaks compatibility with IE and Chrome (at least when using the
    post method).

--- picturelicious/media/opensearch.php
+++ picturelicious/media/opensearch.php
@@ -5,7 +5,7 @@ echo '<?xml version="1.0"?>'."\n";
 echo '<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">'."\n";
 echo '  <ShortName>'.htmlspecialchars(Config::$siteName).'</ShortName>'."\n";
 echo '  <Image width="16" height="16" type="image/x-icon">'.Config::$frontendPath.'media/favicon.ico</Image>'."\n";
-echo '  <Url type="text/html" method="post" template="'.Config::$frontendPath.'search/{searchTerms}" />'."\n";
+echo '  <Url type="text/html" template="'.Config::$frontendPath.'search/{searchTerms}" />'."\n";
 echo '  <Url type="application/opensearchdescription+xml" rel="self" template="'.Config::$frontendPath.'media/opensearch.php" />'."\n";
 echo '</OpenSearchDescription>'."\n";
 ?>
